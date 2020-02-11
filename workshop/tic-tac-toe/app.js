693e2e5a2fc5e846728cb9889e7ceb0a3eeb2752
Manny Dheer <mannydheer@gmail.com>
2020-02-11 10:27:24 -0500

    javascript 7

---
+++ workshop/tic-tac-toe/app.js
@@ -0,0 +1,17 @@
+
+
+//1. Make the board. 
+    //1 board <div>
+    //9 cells <div>
+//2. Cells need to be clickable. 
+    //clicking adds a value. (x or o) to the cell.
+    //** once there us a value, it can't be changed. */
+//3. Clear the board (optional)
+//4. 2 players.
+    //Flag for current player. 
+    //each player is assigned either 'x' or 'o'
+//5. Create an array of current values;
+    //refresh on each change. 
+
+
+
